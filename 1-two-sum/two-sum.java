bda4ebb39e08b4586cdb8d43297ac55a05c1d791
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2023-10-29 16:55:42 -0400

    Time: 39 ms (44.49%) | Memory: 43.6 MB (49.50%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
        int n = nums.length;
-       for (int i=0;i<n;i++){
-           for (int j=i+1; j<n;j++){
-               if (nums[j] + nums[i] == target){
+        for (int i=0;i<n;i++){
+            for (int j=i+1; j<n;j++){
+                if (nums[j] + nums[i] == target){
                     return new int[] { i, j };
-               }
-           }
-       }
-       return new int[] {};
+                }
+            }
+        }
+        return new int[] {};
     }
 }
