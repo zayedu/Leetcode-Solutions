17a72381ff8484f06cef89dd9ec00506f4979442
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2023-10-30 00:22:25 -0400

    Time: 10 ms (63.64%) | Memory: 13.1 MB (81.82%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        max_time = minutesToTest / minutesToDie + 1
+        req_pigs = 0
+        while (max_time) ** req_pigs < buckets:
+             req_pigs += 1
+        return req_pigs 
+        
