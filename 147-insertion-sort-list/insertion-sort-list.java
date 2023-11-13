0f366c0e5550dd0f2942538c0da1c71045b792b3
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2023-11-13 16:50:23 -0500

    Time: 3 ms (94.11%) | Memory: 43.1 MB (61.78%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -1,38 +1,41 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * public class ListListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListListNode next;
+ *     ListListNode() {}
+ *     ListListNode(int val) { this.val = val; }
+ *     ListListNode(int val, ListListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-        public ListNode insertionSortList(ListNode ptr) {    
-        if (ptr == null || ptr.next == null)
-            return ptr;
-        
-        ListNode preInsert, toInsert, dummyHead = new ListNode(0);
-        dummyHead.next = ptr;
-
-        while (ptr != null && ptr.next != null) {
-            if (ptr.val <= ptr.next.val) {
-                ptr = ptr.next;
-            } else {      
-                toInsert = ptr.next;
-                // Locate preInsert.
-                preInsert = dummyHead;
-                while (preInsert.next.val < toInsert.val) {
-                    preInsert = preInsert.next;
+        public ListNode insertionSortList(ListNode head) {    
+      if(head == null) return head;
+        ListNode current = head.next;
+        ListNode pre = head;
+        while(current !=null){
+            if(current.val>=pre.val){
+                current = current.next;
+                pre = pre.next;
+            }
+            else{
+                pre.next = current.next;
+                if(current.val<=head.val){
+                    current.next = head;
+                    head = current;
                 }
-                ptr.next = toInsert.next;
-                // Insert toInsert after preInsert.
-                toInsert.next = preInsert.next;
-                preInsert.next = toInsert;
+                else{
+                    ListNode search = head;
+                    while(search.next != null && search.next.val<current.val){
+                        search = search.next;
+                    }
+                    ListNode tmp = search.next;
+                    search.next = current;
+                    current.next = tmp;
+                }
+                current = pre.next;
             }
         }
-        
-        return dummyHead.next;
+        return head;
     }
 }
