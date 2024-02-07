de3b896f91675038c67f1bce009b2004ff12de73
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-02-06 21:28:05 -0500

    Time: 76 ms (87.80%) | Memory: 16.8 MB (74.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,29 +1,40 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # T: O(log min(m, n)), S: O(1)
-        A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
+        """Finds the median of two sorted arrays.
+        :params:
+            nums1 (List[int]): a sorted list of integer values
+            nums2 (List[int]): a sorted list of integer values
+        :returns:
+            (int) median value of merged nums1 + nums2 array
+        """
+
+        # Binary Search
+        # time:  O(log M + N)
+        # space: O(1)
+        a, b = nums1, nums2
+        if len(b) < len(a):
+            a, b = b, a
+        m, n = len(a), len(b)
+        
+        total = len(a) + len(b)
         half = total // 2
 
-        # Guarantees A is smaller of the two
-        if len(B) < len(A):
-            A, B = B, A
-
-        l, r = 0, len(A) - 1
+        left = 0
+        right = len(a) - 1
         while True:
-            A_i = (l + r) >> 1
-            B_i = half - A_i - 2
+            i = left + (right - left) // 2
+            j = half - i - 2
 
-            A_left = A[A_i] if A_i >= 0 else -inf
-            A_right = A[A_i + 1] if A_i + 1 < len(A) else inf
-            B_left = B[B_i] if B_i >= 0 else -inf
-            B_right = B[B_i + 1] if B_i + 1 < len(B) else inf
+            a_left = a[i] if i >= 0 else -inf
+            a_right = a[i+1] if i+1 < m else inf
+            b_left = b[j] if j >= 0 else -inf
+            b_right = b[j+1] if j+1 < n else inf
 
-            if A_left <= B_right and B_left <= A_right:
+            if a_left <= b_right and b_left <= a_right:
                 if total % 2 == 0:
-                    return (max(A_left, B_left) + min(A_right, B_right)) / 2
-                return min(A_right, B_right)
-            elif A_left > B_right:
-                r = A_i - 1
+                    return (max(a_left, b_left) + min(a_right, b_right)) / 2
+                return min(a_right, b_right)
+            elif a_left > b_right:
+                right = i - 1
             else:
-                l = A_i + 1
+                left = i + 1
