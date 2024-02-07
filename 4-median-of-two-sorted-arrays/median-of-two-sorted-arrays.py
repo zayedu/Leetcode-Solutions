b6d174d92799a5029e927f6e1b432cfd23fa5f06
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-02-06 21:25:03 -0500

    Time: 67 ms (98.84%) | Memory: 16.8 MB (74.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,30 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        def getKthElement(k):
-            beginIndex1, beginIndex2 = 0, 0
-            while True:
-                if beginIndex1 == m:
-                    return nums2[beginIndex2 + k - 1]
-                if beginIndex2 == n:
-                    return nums1[beginIndex1 + k - 1]
-                if k == 1:
-                    return min(nums1[beginIndex1], nums2[beginIndex2]) 
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        total = len(nums1) + len(nums2)
+        half = total//2
+        l, r = 0, len(nums1)-1
+        while True:
+            mid = (l+r) // 2    # mid index of array of short list
+            # total include actual length +2 of merged list , here we have 2 index for 2 
+            #  different list bith include 0 index so for removing that we have to subtract
+            # 2 from comp_mid which is long list.
+            comp_mid = (half -2)- mid
+        
+            leftA = nums1[mid] if mid >=0 else float("-inf")
+            rightA = nums1[mid+1] if (mid+1) < len(nums1) else float("inf")
+            leftB = nums2[comp_mid] if comp_mid >= 0 else float("-inf")
+            rightB = nums2[comp_mid+1] if (comp_mid+1) < len(nums2) else float("inf")
 
-                pivotIndex1 = min(beginIndex1 + k//2 - 1, m - 1)
-                pivotIndex2 = min(beginIndex2 + k//2 - 1, n - 1)
-                pivot1 = nums1[pivotIndex1]
-                pivot2 = nums2[pivotIndex2]
-
-                if pivot1 <= pivot2:
-                    k -= pivotIndex1 - beginIndex1 + 1
-                    beginIndex1 = pivotIndex1 + 1
+            if leftA <= rightB and leftB <= rightA:
+                if total % 2 == 0:
+                    return (max(leftA, leftB)+ min(rightA, rightB))/2
                 else:
-                    k -= pivotIndex2 - beginIndex2 + 1
-                    beginIndex2 = pivotIndex2 + 1             
+                    return min(rightA, rightB)
 
-        m, n = len(nums1), len(nums2)
-        length = m + n
-        if length % 2 == 1:
-            return getKthElement(length//2 + 1)
-        else:
-            return (getKthElement(length//2) + getKthElement(length//2 + 1)) / 2
+            elif leftA > rightB:
+                r = mid -1
+            else:
+                l = mid +1
