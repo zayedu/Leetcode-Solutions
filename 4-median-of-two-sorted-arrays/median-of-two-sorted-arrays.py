18576f2f782b3990f17a967767e599da6c4a77c0
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-02-06 22:10:06 -0500

    Time: 84 ms (57.93%) | Memory: 16.9 MB (59.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,40 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        """Finds the median of two sorted arrays.
-        :params:
-            nums1 (List[int]): a sorted list of integer values
-            nums2 (List[int]): a sorted list of integer values
-        :returns:
-            (int) median value of merged nums1 + nums2 array
-        """
-
-        # Binary Search
-        # time:  O(log M + N)
-        # space: O(1)
-        a, b = nums1, nums2
-        if len(b) < len(a):
-            a, b = b, a
-        m, n = len(a), len(b)
+        A,B=nums1,nums2
+        total =len(nums1)+len(nums2)
+        half=total//2
+        if len(B)<len(A):
+            A,B=B,A
         
-        total = len(a) + len(b)
-        half = total // 2
-
-        left = 0
-        right = len(a) - 1
+        l,r=0,len(A)-1
         while True:
-            i = left + (right - left) // 2
-            j = half - i - 2
+            i=(l+r)//2 #A
+            j=half-i-2 #B
 
-            a_left = a[i] if i >= 0 else -inf
-            a_right = a[i+1] if i+1 < m else inf
-            b_left = b[j] if j >= 0 else -inf
-            b_right = b[j+1] if j+1 < n else inf
-
-            if a_left <= b_right and b_left <= a_right:
-                if total % 2 == 0:
-                    return (max(a_left, b_left) + min(a_right, b_right)) / 2
-                return min(a_right, b_right)
-            elif a_left > b_right:
-                right = i - 1
+            Aleft=A[i] if i>=0 else float('-infinity')
+            Aright=A[i+1] if (i+1)<len(A) else float('infinity')
+            Bleft=B[j] if j>=0 else float('-infinity')
+            Bright=B[j+1]  if (j+1)<len(B) else float('infinity')
+            
+            if Aleft<=Bright and Bleft <=Aright:
+                if total %2 ==1:
+                    return min(Aright,Bright)
+                else:
+                    return (max(Aleft,Bleft)+min(Aright,Bright))/2
+            elif Aleft>Bright:
+                r=i-1
             else:
-                left = i + 1
+                l=i+1
+
+
