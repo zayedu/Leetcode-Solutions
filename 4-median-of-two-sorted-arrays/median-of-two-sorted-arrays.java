5d049d16ce2b7ea7aab6cc2a6915c15b81644d13
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-02-07 21:51:05 -0500

    Time: 1 ms (100.00%) | Memory: 45.9 MB (50.21%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,55 +1,36 @@
-class Solution 
-{
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) 
-    {
-        // O(log(m + n))
-    
-        
-        // let nums1 be the shorter one
-        if (nums2.length < nums1.length) return findMedianSortedArrays(nums2, nums1);
-        
-        // define the number of elements from nums1 that are put into the final left side
-        int lo = 0;
-        int hi = nums1.length;
-        int N = nums1.length + nums2.length;
-        
-        // if N is
-        // odd:  count(left) == count(right) + 1
-        // even: count(left) == count(right)
-        // let assume, the final state is there is p1 elements from nums1
-        // and p2 elements from nums2 and there is:
-        // 1: p1 + p2 == (N + 1) / 2
-        // 2: nums1[p1 - 1] <= nums2[p2]
-        // 3: nums2[p2 - 1] <= nums1[p1]
-        // and the median is:
-        // odd:  Math.max(nums1[p1 - 1], nums2[p2 - 1]) if possible
-        // even: (Math.max(nums1[p1 - 1], nums2[p2 - 1]) + Math.min(nums1[p1], nums2[p2])) / 2.0 if possible
-        // in both above cases, if any index is invalid then just don't use it ..
-        while (lo < hi)
-        {
-            int p1 = (lo + hi) / 2;
-            int p2 = (N + 1) / 2 - p1;
-        
-            if (p1 == 0 || nums1[p1 - 1] <= nums2[p2])
-            {
-                if (p2 == 0 || nums2[p2 - 1] <= nums1[p1]) break;
-                else lo = p1 + 1;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums2.length<nums1.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int m =nums1.length;
+        int n =nums2.length;
+        int start=0;
+        int end =m;
+        while (start<=end){
+            int part1=(start+end)/2;
+            int part2=(m+n+1)/2 -part1;
+            int maxleftnums1=part1==0? Integer.MIN_VALUE:nums1[part1-1];
+            int maxleftnums2=part2==0? Integer.MIN_VALUE:nums2[part2-1];
+            int minrightnums1=part1==nums1.length? Integer.MAX_VALUE:nums1[part1];
+            int minrightnums2=part2==nums2.length? Integer.MAX_VALUE:nums2[part2];
+
+            if(maxleftnums1<=minrightnums2 && maxleftnums2<=minrightnums1){
+                if((m+n)%2==0){
+                    return((Math.max(maxleftnums1,maxleftnums2)+Math.min(minrightnums1,minrightnums2))/2.0);
+                }else{
+                    return Math.max(maxleftnums1,maxleftnums2);
+                }
             }
-            else hi = p1 - 1;
+            else if(maxleftnums1>minrightnums2){
+                end=part1-1;
+            }else{
+                start=part1+1;
+            }
+
+
         }
-        
-        int p1 = (lo + hi) / 2;
-        int p2 = (N + 1) / 2 - p1;
-        int m = Integer.MIN_VALUE;
-        int n = Integer.MAX_VALUE;
-        if (N % 2 == 1)
-        {
-            return Math.max(p1 - 1 >= 0 ? nums1[p1 - 1] : m, p2 - 1 >= 0 ? nums2[p2 - 1] : m);
-        }
-        else
-        {
-            return (Math.max(p1 - 1 >= 0 ? nums1[p1 - 1] : m, p2 - 1 >= 0 ? nums2[p2 - 1] : m) + 
-                    Math.min(p1 < nums1.length ? nums1[p1] : n, p2 < nums2.length ? nums2[p2] : n)) / 2.0;
-        }
+        return 0.0;
     }
+
 }
