a683a43ab8d6874c9f92e31b82ac44f7257f1e0c
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-06-11 20:35:08 -0400

    Time: 38 ms (53.74%) | Memory: 16.6 MB (30.65%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,38 +1,11 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a)>len(b):
-            longerString = a
-            shorterString = b
-        else: 
-            longerString = b
-            shorterString = a
-        
-        while len(shorterString)!=len(longerString):
-            shorterString = "0"+ shorterString
-
-        i = len(longerString) -1
-        carryBit = 0
-        sumBit = 0
-        result = ""
-        while i >= 0:
-            if (carryBit + int(shorterString[i]) + int(longerString[i]) == 3):
-                carryBit = 1
-                result = "1" + result
-
-            elif (carryBit + int(shorterString[i]) + int(longerString[i]) == 2):
-                carryBit = 1
-                result = "0" + result
-            
-            else:
-                result = str(int(shorterString[i])+int(longerString[i])+carryBit) + result
-                carryBit=0
-            
-            i-=1
-
-        if carryBit == 1:
-            result = "1" + result
-
-        return result
-
-
-        
+        i, carry, res, = 0, 0, ''
+        while i < len(a) or i < len(b) or carry:
+            bi1 = int(b[-i-1]) if i < len(b) else 0
+            bi2 = int(a[-i-1]) if i < len(a) else 0
+            num = bi1 + bi2 + carry
+            res = str(num%2) + res
+            carry = 1 if num > 1 else 0
+            i += 1
+        return res
