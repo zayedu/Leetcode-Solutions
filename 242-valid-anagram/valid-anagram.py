ea788425173595b3a5c519f9aa2f9cb18b13f8b2
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-06-27 22:00:02 -0400

    Time: 58 ms (16.10%) | Memory: 16.9 MB (79.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,8 +1,22 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        return sorted(s)==sorted(t)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        my_dict = dict()
+        for i in range (0,len(s)):
+            if s[i] not in my_dict:
+                my_dict[s[i]] = 1 
+            else:
+                my_dict[s[i]] += 1
+
+        for i in range (0,len(t)):
+            if t[i] not in my_dict:
+                return False
+
+            my_dict[t[i]] -= 1
+        
+        print(my_dict)
+
+        for i in my_dict.values():
+            if i != 0:
+                return False
+
+        return True 
