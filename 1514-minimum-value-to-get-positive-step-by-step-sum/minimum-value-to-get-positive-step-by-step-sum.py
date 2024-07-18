4eda26f6d246784753cbf6e0c4ca483b16b68aca
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-07-17 23:05:47 -0400

    Time: 107 ms (5.18%) | Memory: 16.7 MB (19.59%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        init_sv = 1
+
+        
+        sv = 1
+        i = 0
+        while i < len(nums):
+            sv += nums[i]
+            print(sv)
+            if sv < 1:
+                i = 0
+                init_sv += 1
+                sv = init_sv
+                print('###',sv)
+            else:
+                i += 1
+
+        return init_sv
+
+
+
