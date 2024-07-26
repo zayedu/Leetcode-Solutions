3f3d100f324788aafaa929b867a12fb5eec138d3
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-07-25 22:33:50 -0400

    Time: 79 ms (37.63%) | Memory: 15.1 MB (41.98%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,20 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        my_dict = { }
-        for i in range (0,len(nums)):
-            if nums[i] not in my_dict:
-                my_dict[nums[i]] = 1
-            else:
-                my_dict[nums[i]] += 1
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        seen = defaultdict(int)
 
+        '''
+        Seen should look like vals -> occurences
+        '''
+        for i in nums:
+            seen[i] += 1
         result = [ ]
-        
-        for _ in range(k):
-            max_key = max(my_dict, key = my_dict.get)  # Find the key with the maximum value
-            result.append(max_key)
-            my_dict[max_key] = 0  # Remove the key to find the next maximum in the next iteration
-        
+        keys = sorted(seen.items(),reverse=True, key = lambda x:x[1])
+        print(keys)
+        for i in range(k):
+            result.append(keys[i][0])
         return result
