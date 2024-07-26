de4cc69f2e58fbc03dce7ade66bf285c7b22e166
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-07-25 23:15:14 -0400

    Time: 117 ms (34.93%) | Memory: 17.7 MB (46.27%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        arr = [False] * (len(nums) + 1)
+
+        for i in nums:
+            arr[i] = True
+
+        for i in range(len(arr)):
+            if not arr[i]:
+                return i
+        
