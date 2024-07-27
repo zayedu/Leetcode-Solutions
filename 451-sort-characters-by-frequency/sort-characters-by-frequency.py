7dda93d2d0f220525c6b3e01415fb30881d2e12e
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-07-26 21:13:29 -0400

    Time: 55 ms (30.51%) | Memory: 18.5 MB (25.31%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -2,12 +2,46 @@
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        counter = Counter(s)
-        pq = [(-freq, char) for char, freq in counter.items()]
-        heapq.heapify(pq)
-        result = ''
-        while pq:
-            freq, char = heapq.heappop(pq)
-            result += char * -freq
-        return result
+        seen = { }
 
+        '''
+        seen should look like:
+        char -> occurences
+        '''
+
+        for char in s:
+            if char not in seen:
+                seen[char] = 1
+
+            else:
+                seen[char] += 1
+
+        '''
+        seen:
+        t -> 1
+        r -> 1
+        e -> 2
+        
+        seen.items()
+        [(t,1),(r,1),(e,2)]
+        '''             
+
+        sorted_occurences = sorted(seen.items(),reverse = True, key = lambda x:x[1])
+
+        '''
+        sorted_occurences:
+        [(e,2),(t,1),(r,1)]
+        '''
+        result = [ ]
+
+        '''
+        result:
+        []
+        [e,t,r]
+        '''
+
+        for key ,value in sorted_occurences:
+            for addr in range(value):
+                result.append(key)
+        
+        return ''.join(result)
