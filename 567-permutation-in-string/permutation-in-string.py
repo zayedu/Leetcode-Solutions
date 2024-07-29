23aa62ee88f6383364db5a94cfeb910395053efe
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-07-29 19:35:30 -0400

    Time: 90 ms (35.09%) | Memory: 12.1 MB (57.01%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,40 +1,35 @@
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-
-        perm_len = len(s1)
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
         perm_chars = { }
-    
-        for char in s1:
-            if char not in perm_chars:
-                perm_chars[char] = 1
 
+        for s in s1:
+            if s in perm_chars:
+                perm_chars[s] += 1
             else:
-                perm_chars[char] += 1
-
-        #perm_chars contains count of chars in s1
+                perm_chars[s] = 1
 
         l = 0
-
         for r in range(len(s2)):
-            while r - l + 1 > perm_len:
+            while r - l + 1 > len(s1):
                 if s2[l] in perm_chars:
                     perm_chars[s2[l]] += 1
                 l += 1
+            if s2[r] in perm_chars:
+                perm_chars[s2[r]] -= 1
 
             else:
-                if s2[r] in perm_chars:
-                    perm_chars[s2[r]] -= 1
+                while l < r:
+                    if s2[l] in perm_chars:
+                        perm_chars[s2[l]] += 1
 
-                else:
-                    while l < r:
-                        if s2[l] in perm_chars:
-                            perm_chars[s2[l]] += 1
-                        l += 1
+                    l += 1
 
-
-            if max(perm_chars.values()) == 0 and min(perm_chars.values()) == 0:
+            if max(perm_chars.values()) == min(perm_chars.values()) == 0:
                 return True
+
         return False
-
-
-
