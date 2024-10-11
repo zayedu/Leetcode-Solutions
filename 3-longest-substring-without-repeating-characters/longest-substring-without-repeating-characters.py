db61213baad6b54a0dc9911ff95d9efd31ee3571
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-10-11 18:43:04 -0400

    Time: 39 ms (50.03%) | Memory: 12.5 MB (30.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,29 +1,28 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        seen = { }
-        '''
-        seen should look like:
-        char -> last occured index
-        '''
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pos = {
+
+        }
+
+        """
+        pos should look like:
+        char -> position
+        """
+
         l = 0
         max_len = 0
-
-        
         for r in range(len(s)):
-            if s[r] not in seen:
-                seen[s[r]] = r
+            if s[r] not in pos:
+                pos[s[r]] = r
             else:
-                ###Pre conddition: we have a substring with a repeated character
-
-
-                while l <= seen[s[r]]:
-                    
+                while l <= pos[s[r]]:
                     l += 1
-                seen[s[r]] = r
-            ###Post Condition: Substring from l -> r does not have a repeated character
+                pos[s[r]] = r
 
-            max_len = max(max_len,r - l + 1)
+            max_len = max(max_len, r-l+1)
 
         return max_len
