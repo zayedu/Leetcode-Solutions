915cb88e870eec722b04e5a1d32b379f4988f119
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2024-10-17 21:28:55 -0400

    Time: 15 ms (100.00%) | Memory: 16.7 MB (99.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        
+        chars = {
+
+        }
+
+        """
+        chars should look like:
+        char - > freq
+        """
+
+        for char in s:
+            if char not in chars:
+                chars[char] = 1
+            else:
+                chars[char] += 1
+
+        for char in t:
+            if char not in chars:
+                return False
+            else:
+                chars[char] -= 1
+
+        return min(chars.values()) == 0 and max(chars.values())==0 
