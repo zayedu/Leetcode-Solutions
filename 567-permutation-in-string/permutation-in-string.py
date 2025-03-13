0aa3035c518991e820ad4f2ea560dc531d0a196a
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-03-13 15:20:03 -0400

    Time: 43 ms (40.26%) | Memory: 17.9 MB (40.02%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,34 +5,36 @@ class Solution:
 
         }
 
-        """
-        perm_chars which should keep track of freq of chars in s1
-        char -> freq
-        """
+        '''
+        perm_chars should look like:
+            chars -> freq
+        '''
 
-        for char in s1:
-            if char not in perm_chars:
-                perm_chars[char] = 1
+        for c in s1:
+            if c not in perm_chars:
+                perm_chars[c] = 1
+
             else:
-                perm_chars[char] += 1
+                perm_chars[c] += 1
 
+        
         l = 0
-        for r in range(len(s2)):
-            if s2[r] in perm_chars:
-                perm_chars[s2[r]] -= 1
-            else:
-                if s2[l] in perm_chars:
-                    perm_chars[s2[l]] += 1
-                l += 1
-            while (r - l + 1) > len(s1):
-                if s2[l] in perm_chars:
-                    perm_chars[s2[l]] += 1
-                l += 1
-            if min(perm_chars.values()) == 0 and max(perm_chars.values()) == 0:
-                return True
 
+        for r in range(len(s2)):
+            if s2[r] not in perm_chars:
+                while l <= r:
+                    if s2[l] in perm_chars:
+                        perm_chars[s2[l]] += 1
+                    l += 1
+            else:
+                perm_chars[s2[r]] -= 1
+
+                while(perm_chars[s2[r]]<0):
+                    if s2[l] in perm_chars:
+                            perm_chars[s2[l]] += 1
+                    l += 1
+            if max(perm_chars.values()) == 0 and min(perm_chars.values())==0:
+                return True
         return False
 
 
-        
-                
