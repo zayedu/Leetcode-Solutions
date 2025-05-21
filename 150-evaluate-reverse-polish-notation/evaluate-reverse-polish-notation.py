16b2550b0de65b01ecfb80a79a35338a45ac567b
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-21 17:14:27 -0400

    Time: 1 ms (78.55%) | Memory: 19 MB (88.28%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,27 +2,19 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = [ ]
         '''
-        stack should have all numbers, we will always look at the last two nums when we get an operand
+        stack is gonna hold numbers to have stuff done on it
         '''
+        ans = 0
         for token in tokens:
             if token == '+':
-                stack.append(stack.pop()+stack.pop())
+               stack.append(stack.pop() + stack.pop())
             elif token == '-':
                 stack.append(-1*stack.pop()+stack.pop())
             elif token == '*':
                 stack.append(stack.pop()*stack.pop())
             elif token == '/':
-                den = stack.pop()
-                num = stack.pop()
-                stack.append(int(num/den))
+                stack.append(int((1/stack.pop())*stack.pop()))
 
             else:
-                stack.append(int(token))
-
-        return stack.pop()
-        
-        '''
-        Complexities:
-        Time = O(n) 
-        Memory = O(n)
-        '''
+                stack.append(int(token ))
+        return stack[0]
