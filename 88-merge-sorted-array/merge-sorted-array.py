a998142984b669a80a86e37566782f79addbad69
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-21 22:03:57 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.89%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,21 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #nums1[0] = min(nums1), nums1[m-1] = max(nums1)
-        #nums2[0] = min(nums2), nums2[n-1] = max(nums2)
-        #keep smallest, iter other
         i = m-1
         j = n-1
-        k = m+n-1
+        k = n+m -1
 
-        while j >= 0:
-            if i >= 0 and nums2[j] < nums1[i]:
+        while j>=0:
+
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
-            else:
+            else: 
                 nums1[k] = nums2[j]
-                j-=1
+                j -= 1
             k -= 1
-
-            
-
+        
