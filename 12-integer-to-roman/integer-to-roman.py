d77e4be7bca66f745f34518a6d13f1a30e0a0eca
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-21 21:55:22 -0400

    Time: 11 ms (18.66%) | Memory: 17.7 MB (69.99%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,26 +1,25 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         int_to_roman = {
-            1:'I',
-            4:'IV',
-            5:'V',
-            9:'IX',
-            10:'X',
-            40:'XL',
-            50:'L',
-            90:'XC',
-            100:'C',
-            400:'CD',
-            500:'D',
-            900:'CM',
-            1000:'M'
+            1 : 'I',
+            4 : 'IV',
+            5 : 'V',
+            9 : 'IX',
+            10 : 'X',
+            40 : 'XL',
+            50 : 'L',
+            90 : 'XC',
+            100 : 'C',
+            400 : 'CD',
+            500 : 'D',
+            900 : 'CM',
+            1000 : 'M',
         }
+
         result = ''
-        for i,r in reversed(int_to_roman.items()):
-            mult = int(num/i)
+        for value,roman in reversed(int_to_roman.items()):
+            div = int(num/value)
+            result  += div*roman
 
-            result += mult*int_to_roman[i]
-
-            num = num % i
+            num = num%value
         return result
-    
