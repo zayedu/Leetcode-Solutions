ba704818e39d539407bb5a058a037909bdc7da86
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-25 17:48:59 -0400

    Time: 19 ms (37.24%) | Memory: 22.5 MB (31.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,34 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
+        
         anagrams = {
 
         }
 
         '''
         anagrams should look like:
-            anagram_tuple:() -> [words]
-
+            tuple(chars_used) -> [anagrams,..]
         '''
 
-
         for word in strs:
-            char_count = [0] * 26
+            anagram = [0]*26
 
             for char in word:
-                char_count[ord(char)-ord('a')] += 1
+                anagram[ord(char) - ord('a')] += 1
 
-            tupled = tuple(char_count)
+            tupled=tuple(anagram)
 
             if tupled not in anagrams:
                 anagrams[tupled] = [word]
             else:
                 anagrams[tupled].append(word)
 
-        return list(anagrams.values()) 
-        
-        '''
-        Complexity:
-            Time: O(n*k)
-            Memory: O(n*k)
-        '''
+        return list(anagrams.values())
+
+            
