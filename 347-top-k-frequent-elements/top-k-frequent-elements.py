ab7cff1556fa4b7eacde04e6324376862ea60a76
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-25 19:44:15 -0400

    Time: 11 ms (60.11%) | Memory: 14.2 MB (73.35%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,33 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-
-        freq = {
+        freq= {
 
         }
-
-        """
+        '''
         freq should look like:
-        num -> freq
-        """
+            num -> freq
+        '''
 
-        for i in nums:
-            if i not in freq:
-                freq[i] = 1
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
 
             else:
-                freq[i] += 1
+                freq[num] += 1
 
-
-        freq_pairs = freq.items()
-
-        """
-        [(2,2),(1,3),(3,1)]
-        """
-        freq_pairs.sort(reverse = True, key = lambda x:x[1])
+        
+        items = list(freq.items())
+        items.sort(reverse = True, key = lambda x:x[1])
         result = []
-        for idx in range(k):
-            result.append(freq_pairs[idx][0])
+        for count in range(k):
+            result.append(items[count][0])
 
         return result
-        
+
+
