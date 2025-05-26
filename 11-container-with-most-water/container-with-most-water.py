0c7d3e819bcbff7743752c00335b338d02bd34cd
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-26 15:16:16 -0400

    Time: 114 ms (56.43%) | Memory: 20.6 MB (85.87%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,29 +1,21 @@
 class Solution(object):
-    def maxArea(self, height):
+    def maxArea(self, nums):
         """
         :type height: List[int]
         :rtype: int
         """
+        max_volume = 0
 
-        max_wtr = 0
-        l = 0
-        r = len(height) - 1 
-        while l <= r:
-            wtr = ((r-l)*min(height[r],height[l]))
-            max_wtr = max(max_wtr,wtr)
+        l,r = 0,len(nums)-1
 
-            if height[r] > height[l]:
+        while l < r:
+            volume = min(nums[l],nums[r]) * (r-l)
+            max_volume = max(max_volume,volume)
+            if nums[l] < nums[r]:
                 l += 1
 
             else:
-                r -= 1
-        return max_wtr
-        
+                r-=1
 
-        """
-        Complexity:
-        Time: O(n)
-        Space: O(1)
-
-
-        """
+            
+        return max_volume
