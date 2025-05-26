b8301c964bb914802e6f2f57d0367b3f022caddd
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-26 19:41:18 -0400

    Time: 4 ms (13.50%) | Memory: 18.1 MB (26.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,29 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        ans,stack = [ ], [ ]
-        #open < n: +open
-        #open = close = n: return stack
-        #open > close: +close
-        def backtrack(openb,closeb):
-            if openb == closeb and openb == n:
-                ans.append(''.join(stack))
-                return 
-            if openb < n:
-                stack.append('(')
-                backtrack(openb+1,closeb)
-                stack.pop()
+        sols, ans = [],[]
 
-            if openb > closeb:
-                stack.append(')')
+        def backtrack(openb, closeb):
+            if openb == closeb and openb == n:
+                
+                sols.append(''.join(ans))
+                return
+
+            if openb < n:
+                ans.append('(')
+                backtrack(openb+1,closeb)
+                ans.pop()
+
+            if closeb < openb:
+                ans.append(')')
                 backtrack(openb,closeb+1)
-                stack.pop()
+                ans.pop()
 
         backtrack(0,0)
-        return ans
 
-        '''
-        Complexity:
-            Time = O(2^n)
-            Memory = O(k) k is the number of possible parentheses combination
-
-        '''
+        return sols
