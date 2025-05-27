40e799d8368120efb1223fa83f905f76c1387fd3
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-27 19:43:39 -0400

    Time: 120 ms (60.20%) | Memory: 18 MB (86.72%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,30 +1,31 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        seen_freq = {
+        freq_in_window = {
 
         }
 
         '''
-        seen_freq should look like:
-            char_in_window -> freq_in_window
+        freq in window should look like:
+            char -> occurences in window
         '''
 
-        max_freq = 0
-        longest = 0
-
         l = 0
-
+        max_len = 0
+        max_occurences = 0
         for r in range(len(s)):
-            if s[r] not in seen_freq:
-                seen_freq[s[r]] = 1
-            else:
-                seen_freq[s[r]] += 1
-            max_freq = max(max_freq,seen_freq[s[r]])
+            if s[r] not in freq_in_window:
+                freq_in_window[s[r]] = 1
 
-            while r - l + 1 > max_freq + k:
-                seen_freq[s[l]] -= 1
+            else:
+                freq_in_window[s[r]] += 1
+
+            max_occurences = max(max_occurences,freq_in_window[s[r]])
+
+            while r-l+1 - max_occurences > k:
+                freq_in_window[s[l]] -= 1
                 l += 1
 
-            longest = max(longest,r-l+1)
+            max_len = max(max_len,r-l+1)
 
-        return longest
+        return max_len
+    
