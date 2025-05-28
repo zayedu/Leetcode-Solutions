04512f6edc9a163cda85e2c464c74131abb03272
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-05-27 20:02:44 -0400

    Time: 43 ms (40.38%) | Memory: 18 MB (18.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,40 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-        perm_chars = {
+         
+        perm = {
 
         }
 
         '''
-        perm_chars should look like:
-            chars -> freq
+        perm check permutation i.e multiplicity of chars
+        {
+            char(in s1) : freq_in_s1
+        }
         '''
 
-        for c in s1:
-            if c not in perm_chars:
-                perm_chars[c] = 1
-
+        for char in s1:
+            if char not in perm:
+                perm[char] = 1
             else:
-                perm_chars[c] += 1
+                perm[char] += 1
 
-        
+
         l = 0
 
         for r in range(len(s2)):
-            if s2[r] not in perm_chars:
-                while l <= r:
-                    if s2[l] in perm_chars:
-                        perm_chars[s2[l]] += 1
+            if s2[r] in perm:
+                perm[s2[r]] -= 1
+                
+                while perm[s2[r]] < 0:
+                    if s2[l] in perm:
+                        perm[s2[l]] += 1
                     l += 1
+
             else:
-                perm_chars[s2[r]] -= 1
-
-                while(perm_chars[s2[r]]<0):
-                    if s2[l] in perm_chars:
-                            perm_chars[s2[l]] += 1
+                while l <= r:
+                    if s2[l] in perm:
+                        perm[s2[l]] += 1
                     l += 1
-            if max(perm_chars.values()) == 0 and min(perm_chars.values())==0:
+
+            if max(perm.values()) == 0 and min(perm.values()) == 0:
                 return True
+
         return False
-
-
