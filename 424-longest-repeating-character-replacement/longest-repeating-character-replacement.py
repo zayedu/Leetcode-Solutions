b185f3e9cdb0e22b5ec88819ef18b771ec31084d
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-07-27 15:27:06 -0400

    Time: 132 ms (47.31%) | Memory: 18 MB (86.81%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,31 +1,37 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freq_in_window = {
+        freq = {
 
         }
 
         '''
-        freq in window should look like:
-            char -> occurences in window
+        freq should like:
+            char -> freq
         '''
 
-        l = 0
         max_len = 0
-        max_occurences = 0
+        max_freq = 0
+        l = 0
+
         for r in range(len(s)):
-            if s[r] not in freq_in_window:
-                freq_in_window[s[r]] = 1
-
+            
+            if s[r] not in freq:
+                freq[s[r]] = 1
             else:
-                freq_in_window[s[r]] += 1
+                freq[s[r]] += 1
 
-            max_occurences = max(max_occurences,freq_in_window[s[r]])
+            max_freq = max(max_freq, freq[s[r]])
 
-            while r-l+1 - max_occurences > k:
-                freq_in_window[s[l]] -= 1
+            while max_freq + k < (r-l+1) :
+                if s[l] in freq:
+                    freq[s[l]] -= 1
                 l += 1
 
             max_len = max(max_len,r-l+1)
 
         return max_len
-    
+
+        """
+        s = AABABBA
+
+        """
