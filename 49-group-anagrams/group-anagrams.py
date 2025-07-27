cb7c88387fccbcc928f25b7d183c0fe17b1bfebe
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-07-27 12:24:17 -0400

    Time: 16 ms (42.88%) | Memory: 22.6 MB (32.53%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,27 +2,24 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         anagrams = {
-
+            
         }
 
         '''
         anagrams should look like:
-            tuple(chars_used) -> [anagrams,..]
+            (representing count of each alpha char) - > [anagrams,...]
         '''
 
         for word in strs:
-            anagram = [0]*26
-
+            word_list = [0]*26
             for char in word:
-                anagram[ord(char) - ord('a')] += 1
+                word_list[ord(char)-ord('a')] += 1
+            
+            word_list = tuple(word_list)
 
-            tupled=tuple(anagram)
-
-            if tupled not in anagrams:
-                anagrams[tupled] = [word]
+            if word_list not in anagrams:
+                anagrams[word_list] = [word]
             else:
-                anagrams[tupled].append(word)
+                anagrams[word_list].append(word)
 
         return list(anagrams.values())
-
-            
