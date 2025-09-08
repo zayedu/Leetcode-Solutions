0fbb044ac40c6404e5cf13353845f916b81d013e
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-07 23:42:24 -0400

    Time: 40 ms (74.50%) | Memory: 22.3 MB (70.16%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -9,15 +9,15 @@ class Solution:
 
         for char in s:
             if seen_freq_stack and seen_freq_stack[-1][0] == char:
-                seen_freq_stack.append([char,seen_freq_stack[-1][1]+1])
+                seen_freq_stack[-1][1] += 1
             else:
                 seen_freq_stack.append([char,1])
 
             if seen_freq_stack[-1][1] == k:
-                pop_iter = 0
-                while pop_iter < k:
-                    seen_freq_stack.pop()
-                    pop_iter += 1
+                seen_freq_stack.pop()
+        answer = ''
+        for char_info in seen_freq_stack:
+            answer += char_info[1]*char_info[0]
 
-        return ''.join(char_info[0] for char_info in seen_freq_stack)
+        return answer
 
