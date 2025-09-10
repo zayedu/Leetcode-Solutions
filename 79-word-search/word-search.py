b5bd0a8f12a4e2004475dbd1c10120af095dfe72
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-10 00:34:19 -0400

    Time: 5844 ms (14.00%) | Memory: 18.1 MB (21.94%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+
+        '''
+        seen will take (row,col) tuple of board positions
+        '''
+        word_possible = [False]
+        def dfs(row,col, constructed_word, seen,k):
+
+            if (row,col) in seen or row < 0 or row > len(board)-1 or col < 0 or col > len(board[0])-1 or k >= len(word) or board[row][col] != word[k]:
+                return
+
+            seen.add((row,col))
+            constructed_word.append(board[row][col])
+            if ('').join(constructed_word) == word:
+                word_possible.append(True)
+                
+            
+            dfs(row,col+1,constructed_word, seen,k+1)
+            dfs(row-1,col,constructed_word, seen,k+1)
+            dfs(row+1,col,constructed_word, seen,k+1)
+            dfs(row,col-1,constructed_word, seen,k+1)
+
+            constructed_word.pop()
+            seen.remove((row,col))
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    dfs(row,col,[],set(),0)
+
+        return word_possible[-1]
