a99b8dc4fb92bdaa8cb02e3beb9ac473ea4d17a2
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-09 22:30:51 -0400

    Time: 4 ms (41.83%) | Memory: 18.8 MB (50.69%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict = {}
-        for i in range (0,len(nums)):
-            look = target - nums[i]
-            if look in my_dict:
-                return [my_dict[look],i]
-            my_dict[nums[i]] = i
+        seen = { }
 
+        '''
+        seen is a hashmap of:
+            val -> idx
+        '''
+
+        for i in range(len(nums)):
+            
+            if target - nums[i] in seen:
+                return [seen[target-nums[i]],i]
+            else:
+                seen[nums[i]] = i
+
+        return -1
+        
