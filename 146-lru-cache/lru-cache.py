49bfd9a5cccb0993cbf7344c3e8dcfa94403ffb3
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-09 23:50:51 -0400

    Time: 121 ms (77.15%) | Memory: 77.8 MB (91.98%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,25 +1,81 @@
-
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        self.cache = { }
+        '''
+        key -> Node
+        '''
+        self.head = Node(-1,-1)
+        
+
+        self.tail = Node(-1,-1)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.cache.move_to_end(key)          # mark as recently used
-        return self.cache[key]
+
+        if key in self.cache:
+            
+            self.cache[key].prev.next = self.cache[key].next
+            self.cache[key].next.prev = self.cache[key].prev
+
+            self.cache[key].next = self.head.next
+            self.cache[key].prev = self.head
+
+            self.head.next.prev = self.cache[key]
+            self.head.next = self.cache[key]
+
+            return self.cache[key].value
+        
+        return -1
+
 
     def put(self, key: int, value: int) -> None:
-        # Insert or update
-        self.cache[key] = value
-        self.cache.move_to_end(key)          # most recently used
 
-        # Evict if over capacity
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)   # remove least-recently-used
+        if key in self.cache:
+            self.cache[key].value = value
+            
+            self.cache[key].prev.next = self.cache[key].next
+            self.cache[key].next.prev = self.cache[key].prev
 
+            self.cache[key].next = self.head.next
+            self.cache[key].prev = self.head
+
+            self.head.next.prev = self.cache[key]
+            self.head.next = self.cache[key]
+
+            return
+
+        if len(self.cache) >= self.capacity:
+            key_to_delete = self.tail.prev.key
+
+            self.tail.prev = self.tail.prev.prev
+            self.tail.prev.next = self.tail
+
+            del self.cache[key_to_delete]
+
+        value_holding_node = Node(key, value)
+
+        value_holding_node.next = self.head.next
+        value_holding_node.prev = self.head
+
+        self.head.next.prev = value_holding_node
+        self.head.next = value_holding_node
+
+        self.cache[key] = value_holding_node
+
+
+            
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
