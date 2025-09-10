14367fa75ec14c411b06e13f27401847f99e2ff1
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-09 22:26:42 -0400

    Time: 4 ms (68.18%) | Memory: 19.3 MB (32.43%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,31 +1,27 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        connections = {
 
-        }
+        connections = { }
 
-        '''
-        connections should look like:
-            node -> [connected_node1,...,connected_node_n]
+        for i in range(len(graph)):
+            connections[i] = graph[i]
 
-        '''
-        all_possible_paths = [ ]
         last_node = len(graph)
-        for node in range(last_node):
-            connections[node] = graph[node]
+        all_paths = [ ]
 
         def dfs(current_node, path):
+
             if current_node == last_node-1:
-                all_possible_paths.append(path)
-                
+                all_paths.append(path)
+
             else:
                 for edge in connections[current_node]:
+
                     path.append(edge)
-                    dfs(edge, list(path))
+                    dfs(edge,list(path))
                     path.pop()
 
+                
         dfs(0,[0])
 
-        return all_possible_paths
-
-
+        return all_paths
