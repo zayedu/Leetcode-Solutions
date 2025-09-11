a2f67749f0a0a41ef943cfe72665c40e55815715
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-11 00:00:41 -0400

    Time: 3 ms (87.04%) | Memory: 19.9 MB (50.71%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,23 +1,13 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        start = []
-        end = []
-        
-        for s,e in intervals:
-            start.append(s)
-            end.append(e)
-        
-        start.sort()
-        end.sort()
-        room_count = 0 #increment if a meeting starts before one ends
-        end_ptr = 0
-        for s in start:
-            if s < end[end_ptr]: 
-                room_count += 1 
-            else: 
-                end_ptr += 1 
-        
-        return room_count
-
+        intervals.sort()
+        #if start < earliest end at that point, then u have to reserve another room. 
+        heap = []
+        for start,end in intervals:
+            if heap and start >= heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, end)
+                
+        return len(heap)
 
 
