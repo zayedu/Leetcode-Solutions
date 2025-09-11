6e157d021a1b21c99f14ba885c74ab035d9787ae
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-11 17:56:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,20 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+
         closers = {
-            ')':'(',
             '}':'{',
-            ']':'['
+            ')':'(',
+            ']':'['  
         }
-
-        stack = []
-
+        stack = [ ]
         for char in s:
             if char not in closers:
                 stack.append(char)
-            else:
-                if not stack or stack[-1] != closers[char]:
-                    return False
-                stack.pop()
 
-        return len(stack) == 0
+            else:
+
+                if not stack or stack.pop() != closers[char]:
+                    return False
+
+        return not stack
+
+
