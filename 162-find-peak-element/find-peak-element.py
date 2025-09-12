6df8cc2f9b8e2caafee0f14be9427185705c60c4
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-12 18:29:06 -0400

    Time: 0 ms (100.00%) | Memory: 18.6 MB (18.63%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        if n == 1:
+            return 0
+        def binary_search(i):
+
+            if i == 0 and nums[i] > nums[i+1]:
+                return i
+
+            if i == n-1 and nums[i] > nums[i-1]:
+                return i
+
+            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                return i
+
+            if i < n and nums[i+1] > nums[i]:
+                return binary_search(i+1)
+
+            if i > 0 and nums[i-1] > nums[i]:
+                return binary_search(i-1)
+
+        return binary_search(0)
+            
