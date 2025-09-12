0edf55c4631fca77459986d6f1d3566a3b264437
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-12 18:30:25 -0400

    Time: 1 ms (5.70%) | Memory: 18.5 MB (18.63%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -5,7 +5,7 @@ class Solution:
         if n == 1:
             return 0
         def binary_search(i):
-
+            print(i)
             if i == 0 and nums[i] > nums[i+1]:
                 return i
 
@@ -15,11 +15,11 @@ class Solution:
             if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
                 return i
 
-            if i < n and nums[i+1] > nums[i]:
+            if i < n-1 and nums[i+1] > nums[i]:
                 return binary_search(i+1)
 
             if i > 0 and nums[i-1] > nums[i]:
                 return binary_search(i-1)
 
-        return binary_search(0)
+        return binary_search(int(n/2))
             
