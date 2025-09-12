253ea4a64c6e9c2fd6e0f919b94717e31c6baa79
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-11 20:25:06 -0400

    Time: 50 ms (35.29%) | Memory: 20.4 MB (19.75%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,45 +1,40 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.backwards = [ ]
-        self.forwards = [ ]
+
         self.homepage = homepage
-        self.site = homepage
+        self.current_site = homepage
+        self.previous_sites = [ ]
+        self.forward_sites = [ ]
 
     def visit(self, url: str) -> None:
-        self.forwards = [ ]
-        self.backwards.append(self.site)
-        self.site = url
+        self.previous_sites.append(self.current_site)
+        self.current_site = url
+        self.forward_sites = [ ]
 
     def back(self, steps: int) -> str:
-        count = 0
 
-        while count < steps:
-            if len(self.backwards) < 1:
-                return self.homepage
-            self.forwards.append(self.site)
+        for step in range(steps):
             
-            self.site = self.backwards.pop()
-            count+=1
+            if not self.previous_sites:
+                break
 
-        return self.site
+            self.forward_sites.append(self.current_site)
+            self.current_site = self.previous_sites.pop()
 
-        
+        return self.current_site
 
     def forward(self, steps: int) -> str:
-        count = 0
 
-        while count < steps:
-            if len(self.forwards) < 1:
-                return self.site
-            self.backwards.append(self.site)
+        for step in range(steps):
             
-            self.site = self.forwards.pop()
-            count += 1
+            if not self.forward_sites:
+                break
 
-        return self.site
+            self.previous_sites.append(self.current_site)
+            self.current_site = self.forward_sites.pop()
 
-        
+        return self.current_site
         
 
 
