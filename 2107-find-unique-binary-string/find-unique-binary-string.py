38e72b8ac707967765fb4ef27bbcb241f289ba6a
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-13 14:48:46 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.82%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,22 +1,9 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         nums = set(nums)
-        index_to_chars={
 
-        }
         n = len(nums)
-        '''
-        index_to_chars should look like
-            index -> set(chars_at_that_index)
-        '''
 
-        for num in nums:
-            for index in range(len(str(num))):
-                
-                if index not in index_to_chars:
-                    index_to_chars[index] = set()
-
-                index_to_chars[index].add(num[index])
 
 
         def dfs(index,binary):
@@ -27,11 +14,6 @@ class Solution:
                 else:
                     return False
 
-            if '0' not in index_to_chars[index]:
-                return dfs(index+1,binary+'0')
-
-            if '1' not in index_to_chars[index]:
-                return dfs(index+1,binary+'1')
 
             return dfs(index+1,binary+'1') or dfs(index+1,binary+'0')
             
