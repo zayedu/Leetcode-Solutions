fca5890a1d690b5983d261fef7c7b68b8066c895
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-13 23:17:57 -0400

    Time: 6 ms (36.37%) | Memory: 17.8 MB (84.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,24 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res=l1
-        last=0
-        carry=ListNode()
-        while l1 and l2:
-            temp=l1.val+l2.val+carry.val
-            carry.val=temp//10
-            l1.val=temp%10
-            last=l1
-            l1=l1.next
-            l2=l2.next
-        if l2: #if l2 has more nodes than l1
-            last.next=l2
-            l1=l2
-        while l1 and carry.val:
-            temp=l1.val+carry.val
-            carry.val=temp//10
-            l1.val=temp%10
-            last=l1
-            l1=l1.next
-        if carry.val:
-            last.next=carry
-        return res
+    
+
+        temp = ListNode()
+        head = temp
+        prev_carry = 0
+        while l1 or l2:
+            
+            if l1:
+                val1 = l1.val
+                l1=l1.next
+            else:
+                val1 = 0
+            if l2:
+                val2 = l2.val
+                l2=l2.next
+            else:
+                val2 = 0
+
+            if val1+val2+prev_carry >= 10:
+                value = val1+val2-10
+                carry = 1
+            else:
+                value=val1+val2
+                carry = 0
+
+            
+            temp.next = ListNode(value+prev_carry)
+            temp=temp.next
+            prev_carry = carry
+            if carry:
+                temp.next = ListNode(val=carry)
+        return head.next
+            
+                        
