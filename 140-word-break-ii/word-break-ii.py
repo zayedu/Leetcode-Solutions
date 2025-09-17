7b5f246101bcf47e01660ef836416910025d3182
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-16 21:52:51 -0400

    Time: 3 ms (7.44%) | Memory: 17.7 MB (91.76%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,26 +1,24 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        length = len(s)
         wordDict = set(wordDict)
+        sentences = [ ]
+        
+        sentence = [ ]
 
-        ans = [ ]
-        words = [ ]
-        def dfs(i):
-            if i >= len(s):                
-                ans.append(" ".join(words))
+        def backtrack(left_index):
+
+            if left_index == len(s):
+                sentences.append(" ".join(sentence))
                 return
 
+            for right_index in range(left_index,length):
+                if s[left_index:right_index+1] in wordDict:
+                    sentence.append(s[left_index:right_index+1])
+                    backtrack(right_index+1)
+                    sentence.pop()
             
-            for j in range(i,len(s)):
-                if s[i:j+1] in wordDict:
-                    
-                    words.append(s[i:j+1])
-                    dfs(j+1)
-                    words.pop()
+        backtrack(0)
 
-
-        dfs(0)
-
-        return ans
-
-                    
+        return sentences
 
