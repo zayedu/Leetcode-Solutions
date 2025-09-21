f60771346be9aff270986467f4f172ae4ac1274c
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-21 19:32:50 -0400

    Time: 3 ms (87.23%) | Memory: 19.9 MB (50.78%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,13 +1,30 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        #if start < earliest end at that point, then u have to reserve another room. 
-        heap = []
-        for start,end in intervals:
-            if heap and start >= heap[0]:
-                heapq.heappop(heap)
-            heapq.heappush(heap, end)
-                
-        return len(heap)
+        """
+        define an overlap as start time < end time, ONE meeting will have start time < less than
+         end time, and any start time before this end time will result in the allocation of an 
+         additional meeting room
+        """
+        intervals.sort(key= lambda interval:interval[0])
+        start_times = sorted(time[0] for time in intervals)
+        end_times = sorted(time[1] for time in intervals)
+        
+        interval_count = len(intervals)
 
+        start_index = end_index = 0
+        max_rooms = 0
+        running_room_sum = 0
+        while start_index < interval_count and end_index < interval_count:
+            
+            if start_times[start_index] < end_times[end_index]:
+                running_room_sum += 1
+                start_index += 1
+
+            else:
+                running_room_sum -= 1
+                end_index += 1
+
+            max_rooms = max(max_rooms,running_room_sum)
+
+        return max_rooms
 
