90cc228d3ff0e5c2972f400d68c19e63456bf066
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-09-21 21:15:59 -0400

    Time: 5029 ms (24.98%) | Memory: 18 MB (39.68%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,33 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
+        seen = set()
 
-        '''
-        seen will take (row,col) tuple of board positions
-        '''
-        word_possible = [False]
-        def dfs(row,col, constructed_word, seen,k):
+        def dfs(row,col,k):
+            if row < 0 or row > len(board)-1 or col < 0 or col >len(board[0])-1 or (row,col) in seen or word[k] != board[row][col]:
+                return False
 
-            if (row,col) in seen or row < 0 or row > len(board)-1 or col < 0 or col > len(board[0])-1 or k >= len(word) or board[row][col] != word[k]:
-                return
+            
+            if k == len(word)-1:
+                return True
 
             seen.add((row,col))
-            constructed_word.append(board[row][col])
-            if board[row][col] == word[k] and k == len(word)-1:
-                word_possible.append(True)
-                
-            
-            dfs(row,col+1,constructed_word, seen,k+1)
-            dfs(row-1,col,constructed_word, seen,k+1)
-            dfs(row+1,col,constructed_word, seen,k+1)
-            dfs(row,col-1,constructed_word, seen,k+1)
 
-            constructed_word.pop()
+            possible = dfs(row+1,col,k+1) or dfs(row-1,col,k+1) or dfs(row,col+1,k+1) or dfs(row,col-1,k+1)
+
             seen.remove((row,col))
-
+            return possible
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == word[0]:
-                    dfs(row,col,[],set(),0)
+                    if dfs(row,col,0):
+                        return True
 
-        return word_possible[-1]
+        return False
+
+
+                
+
+        
+
