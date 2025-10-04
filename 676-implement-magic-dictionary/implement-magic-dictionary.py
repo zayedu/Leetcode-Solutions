2c5a37ac9c91b14814a31da15499c52868eaac97
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-03 22:39:51 -0400

    Time: 3 ms (100.00%) | Memory: 18 MB (97.41%) - LeetSync

---
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -0,0 +1,41 @@
+class MagicDictionary:
+
+    def __init__(self):
+        self.word_store = defaultdict(set)
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        
+        for word in dictionary:
+            self.word_store[len(word)].add(word)
+
+
+    def search(self, searchWord: str) -> bool:
+        
+        if len(searchWord) not in self.word_store:
+            return False
+
+        for word in self.word_store[len(searchWord)]:
+            if self.diff(word,searchWord) == 1:
+                return True
+            
+        return False
+
+    def diff(self, word1, word2):
+        count = 0
+
+        for index in range(len(word1)):
+            if word1[index] != word2[index]:
+                count += 1
+
+            if count > 1:
+                return count
+
+        return count
+
+
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
