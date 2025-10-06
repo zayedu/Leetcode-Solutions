456a8b1bc996edc8171981c8acc15fe7d169761e
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-05 22:13:26 -0400

    Time: 71 ms (13.49%) | Memory: 20.2 MB (77.99%) - LeetSync

---
+++ 1006-vowel-spellchecker/vowel-spellchecker.py
@@ -0,0 +1,42 @@
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        direct_match = set(wordlist)
+        case_match = {}
+        vowel_match = {}
+
+        for word in wordlist:
+
+            if word.lower() not in case_match:
+                case_match[word.lower()] = word
+            if self.unvowel(word) not in vowel_match:
+                vowel_match[self.unvowel(word)] = word
+
+        spell_checked = []
+        for query in queries:
+            if query in direct_match:
+                spell_checked.append(query)
+
+            elif query.lower() in case_match:
+                spell_checked.append(case_match[query.lower()])
+
+            elif self.unvowel(query) in vowel_match:
+                spell_checked.append(vowel_match[self.unvowel(query)])
+            
+            else:
+                spell_checked.append("")
+
+        return spell_checked
+
+
+    def unvowel(self,word):
+        vowels = {'a','e','i','o','u'}
+        out = []
+        for char in word:
+            if char.lower() in vowels:
+                out.append('*')
+            else:
+                out.append(char.lower())
+        return  ''.join(out)
+
+
+
