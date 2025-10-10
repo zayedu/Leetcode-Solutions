dc5e3e7e23b32b1b6cd3f5e40304282aee3e0862
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-09 20:05:48 -0400

    Time: 140 ms (80.05%) | Memory: 34.1 MB (53.15%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -2,26 +2,45 @@ from sortedcontainers import SortedList
 
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        ans = [2] * len(rains)
+        """
+        index of rains is day
+        rains[index] is lake i.e ans[index]
+        """
+
+
+        ans = [1]*len(rains)
+        rainy_days = {} #day/idx -> day it was filled
         sunny_days = SortedList()
-        full_lakes = {} #lake(idx) -> day filled
 
         for index in range(len(rains)):
             if rains[index] == 0:
                 sunny_days.add(index)
                 continue
-            
-            if rains[index] in full_lakes:
 
-                next_sunny_day = sunny_days.bisect_right(full_lakes[rains[index]])
+            if rains[index] in rainy_days:
+                day_it_rained = rainy_days[rains[index]] #0
+                next_sunny_day = sunny_days.bisect_right(day_it_rained) #1
+
                 if next_sunny_day == len(sunny_days):
-                    return []
-                dry_day = sunny_days[next_sunny_day]
-                ans[dry_day] = rains[index]
-                sunny_days.remove(dry_day)
-
+                     #no sunny day after it last rained i.e we cannot dry this lake before it rains again
+                     return []
+                
+                ans[sunny_days[next_sunny_day]] = rains[index]
+                sunny_days.pop(next_sunny_day)
 
             ans[index] = -1
-            full_lakes[rains[index]] = index 
+            rainy_days[rains[index]] = index
 
         return ans
+        '''
+        rains = [1,2,0,0,2,1]
+                           ^
+        sunny_days = [ 3 ]
+        rainy_day = { 
+            1 : 0,
+            2 : 4,
+        }
+
+        ans = [-1,-1,2,1,1,1]
+        '''
+                
