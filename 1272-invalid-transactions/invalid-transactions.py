5a8157597a410d142535683daf14bfea50494a5c
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-11 14:52:36 -0400

    Time: 75 ms (25.79%) | Memory: 18.4 MB (66.37%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,36 +1,35 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         
-        transaction_mapping = { }
+        transaction_map = defaultdict(list)
 
         '''
-        transaction_mapping should look like:
-            transation_name -> transaction
+        name ->[(timei,cityi,i)]
         '''
 
         possibly_invalid = set()
 
-        for i in range(len(transactions)):
-            transaction_info = transactions[i].split(',')
-            transaction_info.append(i)
-
+        for index in range(len(transactions)):
+            print(transactions[index])
+            transaction_info = transactions[index].split(',')
+            #[name,time,amount,city]
+            print(transaction_info)
             if int(transaction_info[2]) > 1000:
-                possibly_invalid.add(i)
-                
+                possibly_invalid.add(index)
 
-
-            if transaction_info[0] not in transaction_mapping:
-                transaction_mapping[transaction_info[0]] = [transaction_info]
-
-            else:
-                for tran in transaction_mapping[transaction_info[0]]:
-                    if abs(int(tran[1])-int(transaction_info[1])) <= 60 and tran[3]!=transaction_info[3]:
-                        possibly_invalid.add(tran[4])
-                        possibly_invalid.add(i)
-                transaction_mapping[transaction_info[0]].append(transaction_info)
+            if transaction_info[0] in transaction_map:
+                other_trans = transaction_map[transaction_info[0]]
+                for tran in other_trans:
+                    if tran[1] != transaction_info[3] and abs(int(tran[0])-int(transaction_info[1])) <= 60:
+                        possibly_invalid.add(index)
+                        possibly_invalid.add(tran[2])
+            
+            transaction_map[transaction_info[0]].append((transaction_info[1],transaction_info[3],index))
         ans = []
-        for i in possibly_invalid:
-            ans.append(transactions[i])
+        for index in possibly_invalid:
+            ans.append(transactions[index])
+
         return ans
 
+            
 
