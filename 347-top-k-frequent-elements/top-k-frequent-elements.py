8c1e231d1e89754676c8b4580c21fde5262e58ea
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-10 23:35:39 -0400

    Time: 4 ms (95.05%) | Memory: 14.3 MB (56.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,3 @@
-import heapq
-from collections import Counter
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -7,14 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        freq = Counter(nums)
+        
+        freq = defaultdict(int)
 
-        min_heap = []
+        for char in nums:
+            freq[char] += 1
 
-        for num,freq in freq.items():
-            heapq.heappush(min_heap,(freq,num))
-            if len(min_heap)>k:
-                heapq.heappop(min_heap)
-        result = [num for freq ,num in min_heap]
-        return result
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+        items = list(freq.items())
+
+        items.sort(key = lambda x:x[1], reverse = True)
+
+        ans = []
+        
+        for index in range(k):
+            ans.append(items[index][0])
+
+
+        return ans
