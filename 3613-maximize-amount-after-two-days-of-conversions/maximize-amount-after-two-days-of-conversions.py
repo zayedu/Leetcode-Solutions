f731f27f1a648e13e66742bcee5713de9b3f3ee3
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-10 22:30:26 -0400

    Time: 3 ms (98.14%) | Memory: 18 MB (61.25%) - LeetSync

--- 3613-maximize-amount-after-two-days-of-conversions/maximize-amount-after-two-days-of-conversions.py
+++ 3613-maximize-amount-after-two-days-of-conversions/maximize-amount-after-two-days-of-conversions.py
@@ -1,52 +1,37 @@
 class Solution:
     def maxAmount(self, initialCurrency: str, pairs1: List[List[str]], rates1: List[float], pairs2: List[List[str]], rates2: List[float]) -> float:
-        adj_list_1 = defaultdict(list)
-        adj_list_2 = defaultdict(list)
-        for index in range(len(pairs1)):
-            adj_list_1[pairs1[index][0]].append([pairs1[index][1],rates1[index]])
-            adj_list_1[pairs1[index][1]].append([pairs1[index][0],1/rates1[index]])
-        for index in range(len(pairs2)):
-            adj_list_2[pairs2[index][0]].append([pairs2[index][1],rates2[index]])
-            adj_list_2[pairs2[index][1]].append([pairs2[index][0],1/rates2[index]])
+        graph1 = collections.defaultdict(list)
+        graph2 = collections.defaultdict(list)
 
-        max_day_1 = defaultdict(float)
-        max_day_1[initialCurrency] = 1.0
-        seen_d1 = set()
+        for i, (a,b) in enumerate(pairs1):
+            graph1[a].append((b, rates1[i]))
+            graph1[b].append((a, 1/rates1[i]))
 
-        def dfs(node,units):
-            max_day_1[node] = max(max_day_1[node],units)
+        for i, (a,b) in enumerate(pairs2):
+            graph2[a].append((b, rates2[i]))
+            graph2[b].append((a, 1/rates2[i]))
+        
+        def bfs(start, graph):
+            best = collections.defaultdict(float)
+            queue = collections.deque()
+            queue.append((start, 1.0))
+            visited = set()
+            while queue:
+                cur, val = queue.popleft()
+                best[cur] = max(best[cur], val)
+                visited.add(cur)
+                for nei, mult in graph[cur]:
+                    if nei not in visited:
+                        queue.append((nei, val * mult))
+            return best
+        
+        bestconversion1 = bfs(initialCurrency, graph1)
+        bestconversion2 = bfs(initialCurrency, graph2)
 
-            if node in seen_d1:
-                return
-
-            seen_d1.add(node)
-
-            for edge,rate in adj_list_1[node]:
-                dfs(edge,units*rate)
-
-            seen_d1.remove(node)
-
-
-        dfs(initialCurrency,1)
-        max_units = max_day_1[initialCurrency]
-        seen_d2 = set()
-        def dfs_2(node,units):
-            if node == initialCurrency:
-                nonlocal max_units
-                max_units = max(units,max_units)
-                return
-
-            if node in seen_d2:
-                return
-            
-            seen_d2.add(node)
-            for edge,rate in adj_list_2[node]:
-                dfs_2(edge,units*rate)
-
-            seen_d2.remove(node)
-            
-                
-        for node, units in max_day_1.items():
-            dfs_2(node,units)
-            
-        return(max_units)
+        maxconversion = 0
+        for currency in bestconversion1:
+            if currency in bestconversion2:
+                maxconversion = max(maxconversion, bestconversion1[currency] / bestconversion2[currency])
+        
+        return maxconversion
+        
