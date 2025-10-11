b39c12309b535df5ee2488f4a56032425000f747
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-11 15:18:31 -0400

    Time: 3 ms (81.05%) | Memory: 17.9 MB (44.09%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,7 +1,6 @@
 class Solution:
-    def isValidSudoku(self, nums: List[List[str]]) -> bool:
-        
-        rows = {
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows ={
             0: set(),
             1: set(),
             2: set(),
@@ -10,16 +9,10 @@ class Solution:
             5: set(),
             6: set(),
             7: set(),
-            8: set()
+            8: set(),
         }
 
-        '''
-        rows should look like:
-            row_num  -> set of existing nums
-        '''
-
-
-        cols = {
+        cols ={
             0: set(),
             1: set(),
             2: set(),
@@ -28,50 +21,33 @@ class Solution:
             5: set(),
             6: set(),
             7: set(),
-            8: set()
+            8: set(),
         }
 
-        '''
-        cols should look like:
-            col_num -> set of existing nums
-        '''
-        squares = {
-            (0,0) : set(),
-            (0,1) : set(),
-            (0,2) : set(),
-            (1,0) : set(),
-            (1,1) : set(),
-            (1,2) : set(),
-            (2,0) : set(),
-            (2,1) : set(),
-            (2,2) : set(),
+        boxes = {
+            (0,0): set(),
+            (0,1): set(),
+            (0,2): set(),
+            (1,0): set(),
+            (1,1): set(),
+            (1,2): set(),
+            (2,0): set(),
+            (2,1): set(),
+            (2,2): set()
         }
 
-
-        for r in range(len(nums)):
-            for c in range(len(nums[0])):
-                element = nums[r][c]
-                if element == '.':
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                val = board[row][col]
+                if val == '.':
                     continue
-                if element not in rows[r]:
-                    rows[r].add(element)
-                else:
-                    return False
-                    
-                if element not in cols[c]:
-                    cols[c].add(element)
-                else:
-                    return False
-                sq_idx = (c//3,r//3)
+                box = (row//3,col//3)
 
-                if element in squares[sq_idx]:
+                if val in rows[row] or val in cols[col] or val in boxes[box]:
                     return False
-                else:
-                    squares[sq_idx].add(element)
+                
+                rows[row].add(val)
+                cols[col].add(val)
+                boxes[box].add(val)
 
         return True
-
-                
-
-                
-        
