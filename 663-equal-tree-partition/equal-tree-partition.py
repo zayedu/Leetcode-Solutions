3f6752e26697da3193fbb963f40fd81e8378477d
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-11 20:09:49 -0400

    Time: 7 ms (52.78%) | Memory: 21.4 MB (40.28%) - LeetSync

---
+++ 663-equal-tree-partition/equal-tree-partition.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkEqualTree(self, root: Optional[TreeNode]) -> bool:
+        sums = []
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            subtree_sum = node.val + dfs(node.left) + dfs(node.right)
+
+            sums.append(subtree_sum)
+
+            return subtree_sum
+
+        total = dfs(root)
+        sums.pop()
+
+        if total%2 != 0:
+            return False
+        return total//2 in sums 
