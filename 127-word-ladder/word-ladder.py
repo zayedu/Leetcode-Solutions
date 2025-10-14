87495cc981c18508d27c350964e26bf18433c344
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-14 01:18:34 -0400

    Time: 674 ms (16.12%) | Memory: 23.6 MB (14.61%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,47 +1,50 @@
 from collections import deque
 
 class Solution:
+
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        word_set = set(wordList)
-        if endWord not in word_set:
-            return 0
 
-        def anon_word(word):
-            anon_words = []
-
+        def anon_words(word):
+            ans = []
             for index in range(len(word)):
-                anon_word = list(word)
-                anon_word[index] = '*'
-                anon_words.append(''.join(anon_word))
-            
-            return anon_words
-        
-        word_matches = defaultdict(list)
+                word_list = list(word)
+                word_list[index] = '*'
+                ans.append(''.join(word_list))
+
+            return ans
+
+        word_set = set(wordList)
+
+        if endWord not in word_set:
+            return 0 
+
+
+        adj_list = defaultdict(list)
 
         for word in wordList:
-            for index in range(len(word)):
-                word_anon = list(word)
-                word_anon[index] = '*'
-                word_matches["".join(word_anon)].append(word)
+            for anon_word in anon_words(word):
+                adj_list[anon_word].append(word)
 
 
-            
+    #bfs
+        seen = set()
         queue = deque([[beginWord,0]])
-        visited = set()
+
         while queue:
+            
+            word, steps = queue.popleft()
 
-            word, moves = queue.popleft()
             if word == endWord:
-                return moves+1
+                return steps +1
 
-            anon_words = anon_word(word)
+            seen.add(word)
+            a_words = anon_words(word)
 
-            for a_word in anon_words:
-                for edge in word_matches[a_word]:
-                    if edge in visited:
-                        continue
-                    visited.add(edge)
-                    queue.append([edge,moves+1])
-                    
-                    
+            for anon_word in a_words:
+                for edge in adj_list[anon_word]:
+                    if edge not in seen:
+                        queue.append([edge,steps+1])
+
         return 0
+
+
