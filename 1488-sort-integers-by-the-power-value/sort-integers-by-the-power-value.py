efcb6ce532231ffb73c7590a8d949f976cac88e2
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-15 18:31:13 -0400

    Time: 451 ms (22.75%) | Memory: 18 MB (81.74%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -6,9 +6,10 @@ class Solution:
                 return power
 
             if num %2==0:
-                return getPower(num/2,power+1)
+                return getPower(num//2,power+1)
             
             return getPower(3*num+1,power+1)
+
         num_power = defaultdict(int)
 
         '''
@@ -18,8 +19,23 @@ class Solution:
         for num in range(lo,hi+1):
             num_power[num] = getPower(num,0)
 
-        items = list(num_power.items())
-        items.sort(key= lambda x:x[1])
+        ans = [[] for _ in range(max(num_power.values())+1)]
 
-        return items[k-1][0]
+        for num, power in num_power.items():
+            ans[power].append(num)
+
+        count = 0
+
+        for index in range(len(ans)):
+            if not ans[index]:
+                continue
+
+            if count + len(ans[index]) >= k:
+                vals = sorted(ans[index])
+                
+                return vals[k-count-1]
+
+            count += len(ans[index])
+
+        
             
