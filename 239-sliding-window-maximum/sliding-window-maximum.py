a93e6405721a5f648a15e87dff0bc0cd3b4898fe
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-14 20:03:10 -0400

    Time: 155 ms (78.38%) | Memory: 31 MB (87.18%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -7,20 +7,22 @@ class Solution:
 
         l = 0
         ans = []
+
         for r in range(len(nums)):
-            
+
+
             while queue and nums[queue[-1]] < nums[r]:
                 queue.pop()
+
             queue.append(r)
 
             while queue[0] < l:
                 queue.popleft()
 
-            if r < k-1:
+            if r < k -1:
                 continue
-
-            ans.append(nums[queue[0]])
             l+=1
-
+            ans.append(nums[queue[0]])
 
         return ans
+        
