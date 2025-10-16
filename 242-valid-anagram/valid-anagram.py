56d5fe20367650eba761d0fc8f16ca4038604692
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-16 19:41:44 -0400

    Time: 13 ms (48.03%) | Memory: 17.7 MB (86.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,26 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        char_count = {
-
-        }
-
-        '''
-        char_count is 
-            char -> frequency_in_s
-        '''
+        s_count = defaultdict(int)
 
         for char in s:
-            if char in char_count:
-                char_count[char] += 1
-
-            else:
-                char_count[char] = 1
+            s_count[char] += 1
 
         for char in t:
-            if char not in char_count or char_count[char] <= 0:
+            if char not in s_count or s_count[char] ==0:
                 return False
-            
-            char_count[char] -= 1
-            
-        return all(v==0 for v in char_count.values())
+            s_count[char] -= 1
+        
+        return (min(s_count.values())==0 == max(s_count.values()))
