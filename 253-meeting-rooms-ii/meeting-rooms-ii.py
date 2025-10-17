5550713f701a18ba047af7eee9fdbc7e33943b0d
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-17 19:13:51 -0400

    Time: 4 ms (54.86%) | Memory: 19.9 MB (50.99%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,30 +1,26 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        """
-        define an overlap as start time < end time, ONE meeting will have start time < less than
-         end time, and any start time before this end time will result in the allocation of an 
-         additional meeting room
-        """
-        intervals.sort(key= lambda interval:interval[0])
-        start_times = list(time[0] for time in intervals)
-        end_times = sorted(time[1] for time in intervals)
+        starts = sorted([interval[0] for interval in intervals])
+        ends = sorted([interval[1] for interval in intervals])
         
-        interval_count = len(intervals)
+        meetings = len(intervals)
 
-        start_index = end_index = 0
-        max_rooms = 0
-        running_room_sum = 0
-        while start_index < interval_count and end_index < interval_count:
-            
-            if start_times[start_index] < end_times[end_index]:
-                running_room_sum += 1
+        start_index = 0
+        end_index = 0
+
+        max_overlaps = 0
+        running_overlaps = 0
+
+        while start_index < meetings and end_index < meetings:
+
+            if starts[start_index] < ends[end_index]:
+                running_overlaps += 1
                 start_index += 1
-
             else:
-                running_room_sum -= 1
+                running_overlaps -= 1
                 end_index += 1
 
-            max_rooms = max(max_rooms,running_room_sum)
+            max_overlaps = max(max_overlaps,running_overlaps)
 
-        return max_rooms
+        return max_overlaps
 
