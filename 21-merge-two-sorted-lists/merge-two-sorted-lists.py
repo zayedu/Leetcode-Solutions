830f083c8788ba422c6210524fe21c79cf59438d
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-16 21:29:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.40%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,20 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import sys
-sys.setrecursionlimit(1000000)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = ListNode()
-        while list1 and list2:               
+        
+        temp = ListNode()
+        new_head = temp
+
+        while list1 and list2:
+
             if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+                temp.next = list1
+                list1=list1.next
             else:
-                cur.next = list2
-                list2, cur = list2.next, list2
-                
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-            
-        return dummy.next
+                temp.next = list2
+                list2=list2.next
+
+            temp = temp.next
+
+        if list1:
+            temp.next = list1
+
+        if list2:
+            temp.next = list2
+
+        return new_head.next
+
