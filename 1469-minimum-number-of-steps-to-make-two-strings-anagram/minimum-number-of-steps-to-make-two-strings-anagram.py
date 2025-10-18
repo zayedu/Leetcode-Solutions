5394df3ed9a93f125e8d4252828fe1e6e678b558
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-17 21:51:14 -0400

    Time: 140 ms (18.68%) | Memory: 17.8 MB (99.50%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,16 +1,13 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-
-        counts = [0]*26
+    
+        count = [0] *26
 
         for char in s:
-            counts[ord(char)-ord('a')] += 1
+            count[ord(char)-ord('a')]+=1
 
         for char in t:
-            if counts[ord(char)-ord('a')]:
-                counts[ord(char)-ord('a')] -= 1
-            # else:
-            #     counts[ord(char)-ord('a')] += 1
+            if count[ord(char)-ord('a')]:
+                count[ord(char)-ord('a')]-=1
 
-        return sum(counts)
-
+        return sum(count)
