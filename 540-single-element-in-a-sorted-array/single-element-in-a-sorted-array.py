9279b39a8c753511b0bc099997d22b4beb3972ef
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-19 18:13:47 -0400

    Time: 0 ms (100.00%) | Memory: 24.8 MB (44.63%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -6,8 +6,8 @@ class Solution:
             mid= (l+r)//2
             if mid %2:
                 mid-=1
-                
-            if mid%2==0 and nums[mid+1] == nums[mid]:
+
+            if nums[mid+1] == nums[mid]:
                 l = mid +2
             else:
                 r= mid-1
