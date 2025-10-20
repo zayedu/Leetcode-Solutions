1a55d280fcb938119bbe99e93f96d5a45d7861dc
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-20 17:36:52 -0400

    Time: 246 ms (57.96%) | Memory: 20.2 MB (68.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,14 +1,12 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        seen = set() #(row,col)
-
         def dfs(row,col):
 
-            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0' or (row,col) in seen:
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0' :
                 return
             
-            seen.add((row,col))
+
             grid[row][col]='0'
 
             dfs(row+1,col)
@@ -16,7 +14,7 @@ class Solution:
             dfs(row,col-1)
             dfs(row,col+1)
 
-            seen.remove((row,col))
+
 
         islands = 0
 
