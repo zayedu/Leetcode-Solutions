6d8aabf380092cc7ba23bb9d84e7e969fbd6f793
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-20 16:23:18 -0400

    Time: 125 ms (65.25%) | Memory: 58 MB (40.84%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,39 +1,43 @@
-import random
-import heapq  # if you had this elsewhere; harmless to leave/remove
-
 class RandomizedSet:
 
     def __init__(self):
-        self.size = 0
-        self.values = []
-        self.values_map = {}
+        self.vals = []
+        self.vals_map = {}
 
     def insert(self, val: int) -> bool:
-        if val in self.values_map:
+        if val in self.vals_map:
             return False
-        self.values.append(val)
-        self.size += 1
 
-        self.values_map[val] = self.size - 1
+        self.vals.append(val)
+        self.vals_map[val] = len(self.vals)-1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.values_map:
+        
+        if val not in self.vals_map:
             return False
 
-        idx = self.values_map[val]
-        
-        last_val = self.values[self.size - 1]
+        if val == self.vals[-1]:
+            del self.vals_map[val]
+            self.vals.pop()
+            return True
 
-        if idx != self.size-1:
-            self.values[idx] = last_val
-            self.values_map[last_val] = idx
- 
-        self.values.pop()
-        self.size -= 1
-        del self.values_map[val]
+        index_to_replace = self.vals_map[val]
+        self.vals[index_to_replace] = self.vals[-1]
+        self.vals.pop()
+        self.vals_map[self.vals[index_to_replace] ] = index_to_replace
+        del self.vals_map[val]
+
         return True
-
+        
+    
     def getRandom(self) -> int:
-        random_index = random.randint(0, self.size - 1)
-        return self.values[random_index]
+        
+        return self.vals[randint(0,len(self.vals)-1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
