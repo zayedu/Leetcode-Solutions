b052e52b6c7bab85c3351c7a99a47a58bb66fdd4
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-20 21:26:00 -0400

    Time: 11 ms (71.30%) | Memory: 19.1 MB (93.08%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,30 +3,21 @@ class Solution:
         
         l,r = 0,len(height)-1
 
-        left_wall,right_wall = height[l],height[r]
-        water  = 0
+        left_wall,right_wall = height[0],height[r]
+
+        water = 0 
+
         while l < r:
-            if left_wall < right_wall:
+            if right_wall > left_wall:
                 l += 1
-                water += left_wall - height[l] if left_wall - height[l] > 0 else 0
+                water += left_wall - height[l] if left_wall - height[l] >= 0 else 0
+
+                left_wall = max(left_wall,height[l])
 
             else:
-                r -= 1 
-                water += right_wall - height[r] if right_wall - height[r] > 0 else 0
+                r-=1 
+                water += right_wall - height[r] if right_wall - height[r] >=0 else 0
 
-            left_wall = max(left_wall,height[l])
-            
-            right_wall = max(right_wall,height[r])
+                right_wall = max(right_wall,height[r])
 
         return water
-
-
-        """
-        height = [0,1,0,2,1,0,1,3,2,1,2,1]
-                          ^     ^
-
-        left_wall = 1 
-        right_wall = 3
-
-        water = 5
-        """
