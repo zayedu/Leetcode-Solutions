9425a58b31d1fe1121efd8a6c3f88643ab141940
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-20 21:05:15 -0400

    Time: 610 ms (17.44%) | Memory: 23.5 MB (15.15%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,51 +1,45 @@
 from collections import deque
 
 class Solution:
-
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        @lru_cache()
+        if endWord not in wordList:
+            return 0
+
         def anon_words(word):
-            ans = []
+            words = []
+
             for index in range(len(word)):
                 word_list = list(word)
+
                 word_list[index] = '*'
-                ans.append(''.join(word_list))
 
-            return ans
+                words.append(''.join(word_list))
 
-        word_set = set(wordList)
-
-        if endWord not in word_set:
-            return 0 
-
+            return words
 
         adj_list = defaultdict(list)
 
         for word in wordList:
-            for anon_word in anon_words(word):
+            a_words = anon_words(word)
+
+            for anon_word in a_words:
                 adj_list[anon_word].append(word)
 
-
-    #bfs
-        seen = set()
         queue = deque([[beginWord,1]])
-
+        seen = set()
         while queue:
-            
+
             word, steps = queue.popleft()
 
             if word == endWord:
                 return steps
 
-
             a_words = anon_words(word)
+
+            seen.add(word)
 
             for anon_word in a_words:
                 for edge in adj_list[anon_word]:
                     if edge not in seen:
-                        seen.add(edge)
                         queue.append([edge,steps+1])
-
         return 0
-
-
