2fa90ad5e3c49c891ebc87c6868ec5ae124aa98f
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2025-10-20 20:17:44 -0400

    Time: 4 ms (56.66%) | Memory: 17.7 MB (92.39%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,53 +1,55 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows ={
-            0: set(),
-            1: set(),
-            2: set(),
-            3: set(),
-            4: set(),
-            5: set(),
-            6: set(),
-            7: set(),
-            8: set(),
+        
+        rows = {
+            0 : set(),
+            1 : set(),
+            2 : set(),
+            3 : set(),
+            4 : set(),
+            5 : set(),
+            6 : set(),
+            7 : set(),
+            8 : set()
         }
 
-        cols ={
-            0: set(),
-            1: set(),
-            2: set(),
-            3: set(),
-            4: set(),
-            5: set(),
-            6: set(),
-            7: set(),
-            8: set(),
+        cols = {
+            0 : set(),
+            1 : set(),
+            2 : set(),
+            3 : set(),
+            4 : set(),
+            5 : set(),
+            6 : set(),
+            7 : set(),
+            8 : set()
         }
 
-        boxes = {
-            (0,0): set(),
-            (0,1): set(),
-            (0,2): set(),
-            (1,0): set(),
-            (1,1): set(),
-            (1,2): set(),
-            (2,0): set(),
-            (2,1): set(),
-            (2,2): set()
+        squares = {
+            (0,0) : set(),
+            (0,1) : set(),
+            (0,2) : set(),
+            (1,0) : set(),
+            (1,1) : set(),
+            (1,2) : set(),
+            (2,0) : set(),
+            (2,1) : set(),
+            (2,2) : set()
         }
 
         for row in range(len(board)):
-            for col in range(len(board[row])):
-                val = board[row][col]
-                if val == '.':
+            for col in range(len(board[0])):
+
+                value = board[row][col]
+                if value == '.':
                     continue
                 box = (row//3,col//3)
 
-                if val in rows[row] or val in cols[col] or val in boxes[box]:
+                if value in rows[row] or value in cols[col] or value in squares[box]:
                     return False
-                
-                rows[row].add(val)
-                cols[col].add(val)
-                boxes[box].add(val)
+
+                rows[row].add(value)
+                cols[col].add(value) 
+                squares[box].add(value) 
 
         return True
