741b3582a19d74fb372610d138d1b3e1b2aeb5dd
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2026-07-14 20:49:50 -0700

    Time: 122 ms (64.62%) | Memory: 19.1 MB (90.99%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,16 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        cnt = 0
+        max_count = 0
 
-        def _is_palindrome(l, r):
-            nonlocal cnt
+        def _is_palindrome(l,r):
+            count = 0
+
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                    l -= 1
-                    r += 1
-                    cnt += 1
-    
-        for i in range(len(s)):
-            _is_palindrome(i, i)
-            _is_palindrome(i, i + 1)
-        
-        return cnt
+                count += 1
+                l -= 1
+                r += 1
+
+            return count
+
+        for index in range(len(s)):
+            max_count += _is_palindrome(index,index) + _is_palindrome(index,index+1)
+
+        return max_count
+                
