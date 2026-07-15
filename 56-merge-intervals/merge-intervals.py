8a207f16f499c8b36fd90e9c9932ddb517075a3c
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2026-07-14 22:49:31 -0700

    Time: 7 ms (72.15%) | Memory: 22.2 MB (97.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,13 +1,20 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-        intervals.sort(key=lambda interval:interval[0])
+        """
+        sort by start time
+        use a stack and go up, update head interval until you find a start time after your end time and then new stack
+        """
+
+        intervals.sort(
+            key = lambda interval: interval[0]
+        )
 
         stack = []
 
         for interval in intervals:
             if stack and stack[-1][1] >= interval[0]:
-                stack[-1][1] = max(stack[-1][1],interval[1])
+                stack[-1][1] = max(interval[1], stack[-1][1])
             else:
                 stack.append(interval)
 
