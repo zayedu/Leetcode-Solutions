e51b909efb247481df5b7c46768075d1513d1e16
Zayed Umer <115606672+zayedu@users.noreply.github.com>
2026-07-15 22:51:29 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (38.97%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,19 +1,18 @@
 from collections import deque
+
 class HitCounter:
 
     def __init__(self):
-        self.queue = deque()
+        self.queue = deque([])
         
 
     def hit(self, timestamp: int) -> None:
         self.queue.append(timestamp)
         
-
     def getHits(self, timestamp: int) -> int:
-        count = 0
-        while self.queue and abs(timestamp - self.queue[0]) >= 300:
+
+        while self.queue and (timestamp-self.queue[0]) >= 300:
             self.queue.popleft()
-            count +=1
 
         return len(self.queue)
         
